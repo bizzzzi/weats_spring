4d532608fdae3cf196cdef17eb8c32d233718452
choiseongrak <csr961120@gmail.com>
2020-11-30 17:09:46 +0900

    회원 탈퇴 및 비밀번호 변경

---
+++ src/main/java/com/controller/MemberManagerController.java
@@ -0,0 +1,80 @@
+package com.controller;
+
+import com.dto.MemberDTO;
+import com.encrypt.SHA256;
+import com.encrypt.UserVerify;
+import com.service.MemberService;
+import com.sun.xml.internal.ws.resources.HttpserverMessages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class MemberManagerController {
+
+    @Autowired
+    MemberService service;
+
+    @Autowired
+    UserVerify userVerify;
+
+    @GetMapping("/loginCheck/passwdCheck")
+    public String passwdCheckPage(String page, HttpSession session) {
+        if(page != null) {
+            session.setAttribute("page", page);
+        }
+        return "passwdCheck/passwdCheck";
+    }
+    @PostMapping("/loginCheck/passwdCheck")
+    public String passwdCheck(String user_pw, HttpSession session) {
+        String page = (String)session.getAttribute("page");
+        MemberDTO login_dto = (MemberDTO) session.getAttribute("login");
+        String user_email = login_dto.getUser_email();
+
+        MemberDTO dto = userVerify.verify(user_email, user_pw);
+
+        String next = "";
+        if(dto != null) { //비번 인증 완료
+            System.out.println("비번 인증 완료");
+
+            if(page.equals("delete")) { //회원 탈퇴 클릭시 넘어오는 page 키의 value
+                service.memberDelete(user_email);
+                System.out.println("회원 탈퇴 완료");
+                session.removeAttribute("login");
+                next = "redirect:/";
+            } else if(page.equals("pwchange")) { //비밀번호 변경 버튼 클릭 시
+                next = "redirect:/passwdChange";
+            }
+        } else { //비번 인증 실패 시
+            session.setAttribute("mesg", "비밀번호를 잘못 입력하셨습니다.");
+            next = "redirect:passwdCheck";
+        }
+        return next;
+    }
+
+    @PostMapping("/loginCheck/passwdChange")
+    public String passwdChacge(HttpSession session, @RequestParam("new_pw") String pw) {
+        MemberDTO login_dto = (MemberDTO) session.getAttribute("login");
+        String user_email = login_dto.getUser_email();
+        String salt = service.getSaltMember(user_email);
+        System.out.println(user_email);
+        System.out.println(salt);
+        String new_pw = SHA256.getEncrypt(pw, salt);
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("user_email", user_email);
+        map.put("user_pw",new_pw);
+        service.pwUpdate(map);
+
+        return "redirect:/MainAccountManagement";
+    }
+
+
+}
--- src/main/webapp/WEB-INF/views/member/accountManagement.jsp
+++ src/main/webapp/WEB-INF/views/member/accountManagement.jsp
@@ -1,23 +1,17 @@
-<%@page import="com.dto.MemberDTO"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<%
-	MemberDTO dto = (MemberDTO) session.getAttribute("login");
-	String username = dto.getUser_name();
-	String useremail = dto.getUser_email();
-%>
 <form>
 	<div class="userName">
 		<span>이름:</span>
-		<span><%=username %></span>
+		<span>${login.user_name}</span>
 	</div>
 	<div class="userEmail">
 		<span>이메일</span>
-		<span><%=useremail %></span>
+		<span>${login.user_email}</span>
 	</div>
 	<div>
-		<a href="passwdCheckredirect?page=delete">회원탈퇴</a>
-		<a href="passwdCheckredirect?page=pwchange">비밀번호 변경</a>
+		<a href="loginCheck/passwdCheck?page=delete">회원탈퇴</a>
+		<a href="loginCheck/passwdCheck?page=pwchange">비밀번호 변경</a>
 	</div>
 </form>
