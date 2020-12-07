88535c830d304e2a68c6dc34d92e8382c4155094
mikyungyun <yun.workk@gmail.com>
2020-12-07 15:40:44 +0900

    추천페이지

--- src/main/webapp/resources/js/slider.js
+++ src/main/webapp/resources/js/slider.js
@@ -1,22 +1,21 @@
 // 추천 슬라이더
 
-var swiper = new Swiper('.swiper-container', {
-	slidesPerView : 3,
-	spaceBetween : 100,
-	slidesPerGroup : 3,
-	loop : true,
-	loopFillGroupWithBlank : true,
-	speed : 600,
-	pagination : {
-		el : '.swiper-pagination',
-		clickable : true,
-	},
-	navigation : {
-		nextEl : '.swiper-button-next',
-		prevEl : '.swiper-button-prev',
-	},
-	autoplay: {
-	    delay: 3000,
-	  },
-	
-});
+let swiper = new Swiper('.swiper-container', {
+      slidesPerView: 4,
+      spaceBetween: 25,
+      slidesPerGroup: 4,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      speed: 900,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      autoplay: {
+        delay: 3000,
+      },
+    });
