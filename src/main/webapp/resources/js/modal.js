f09a96d4e8fb4d33811a0e575e8841d1712b9c17
Hyeongjin Kim <kimjin5537@daum.net>
2020-12-15 16:05:14 +0900

    비밀번호찾기 모달 & 이메일 유효성 검사 비동기처리

--- src/main/webapp/resources/js/modal.js
+++ src/main/webapp/resources/js/modal.js
@@ -36,3 +36,58 @@
 	overlay2.addEventListener("click", closeModal2);
 	closeBtn2.addEventListener("click", closeModal2);
 	signUpOpenBtn.addEventListener("click", openModal2);
+	
+// 비밀번호 찾기 모달
+	
+	const findPw = document.querySelector('#findPw');
+	const modal3 = document.querySelector(".findPwModal");
+	const overlay3 = document.querySelector(".modalOverlay3");
+	const closeBtn3 = document.querySelector(".closeBtn3");
+	
+	const openModal3 = () => {
+		modal3.classList.remove("hidden");
+		modal.classList.add("hidden");
+	}
+	
+	const closeModal3 = () => {
+		  modal3.classList.add("hidden");
+		}
+		
+	
+	overlay3.addEventListener("click", closeModal3);
+	closeBtn3.addEventListener("click", closeModal3);
+	findPw.addEventListener("click", openModal3);
+	
+	$(".userEmail").on("keyup", function() {
+		console.log("XXXX");
+		$.ajax({
+			type : "GET",
+			url : "emailCheck", // 서버 주소
+			data : {
+				user_email : $(".userEmail").val(), // 전송 데이터
+			},
+			dataType : "text", // 응답 데이터 타입
+			success : function(data, status, xhr) {
+				console.log(data);
+				$("#findEmailResult").text(data);
+			},
+			error : function(xhr, status, error) {
+				console.log("error");
+			}
+		});
+	});// end-
+	
+	
+	const findEmailResult = document.querySelector('#findEmailResult');
+	const ajax_findPw = document.querySelector('#ajax_findPw');
+	const findUserEmail = document.querySelector('.userEmail');
+	
+	ajax_findPw.addEventListener("submit", function(e) {
+		if (findEmailResult.innerText != "이메일 중복") {
+			alert("존재하지 않는 이메일입니다");
+			findUserEmail.value = null;
+			findUserEmail.focus();
+			e.preventDefault();
+		}
+	})
+	
