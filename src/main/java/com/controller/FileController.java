e005b4d92e93e6a223c0e46eb9842b791661d4cd
bizzzzi <mungzzi83@gmail.com>
2020-12-22 04:57:10 +0900

    중고거래 > 댓글 기능 에러 수정, 게시물 수정 / 삭제 기능 구현

--- src/main/java/com/controller/FileController.java
+++ src/main/java/com/controller/FileController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -81,4 +82,5 @@ public class FileController {
         }
         return new ResponseEntity<String>("deleted", HttpStatus.OK);
     }
+
 }
