65f7df4adb1b975b1fe5d466dce3468251348fff
choiseongrak <csr961120@gmail.com>
2020-12-23 15:01:36 +0900

    예약 내역 수정

--- src/main/java/com/dao/ReserveDAO.java
+++ src/main/java/com/dao/ReserveDAO.java
@@ -54,10 +54,6 @@ public class ReserveDAO {
         template.update("reviewVerify", map);
     }
 
-//    public void reviewUpdate(Map<String,?> map) {
-//        template.update("reviewUpdate", map);
-//    }
-
     public void reviewDelete(Map<String, String> map) {
         System.out.println(map);
         template.delete("reviewDelete", map);
@@ -68,19 +64,11 @@ public class ReserveDAO {
         template.insert("reserveItemAdd", itemDTOList);
     }
 
-//    public void deleteReserveItem(String reservation_id) {
-//        template.delete("deleteReserveItem",reservation_id);
-//    }
-//
-//    public List<String> reserveIdByDate(Map<String, String> map) {
-//        return template.selectList("reserveIdByDate", map);
-//    }
-//
-//    public List<ReservationItemDTO> personCount(List<String> list) {
-//        return template.selectList("personCount", list);
-//    }
-
     public List<HashMap<String,?>> reservePerson(Map<String, String> map) {
         return template.selectList("reservePerson", map);
+    }
+
+    public List<ReservationItemDTO> reserveDetailList(String reservation_id) {
+        return template.selectList("reserveDetailList",reservation_id);
     }
 }
