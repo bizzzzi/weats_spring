8edc63b66531689c2abc056a5ffc50c9fa7c42bd
csy <gultree99@naver.com>
2020-12-30 13:07:47 +0900

    유효성검사

--- src/main/webapp/resources/js/partnerInput.js
+++ src/main/webapp/resources/js/partnerInput.js
@@ -4,7 +4,7 @@ let sample4_postcode=document.getElementById('sample4_postcode');
 let sample4_roadAddress=document.getElementById('sample4_roadAddress');
 let sample4_jibunAddress=document.getElementById('sample4_jibunAddress');
 let partner_license_num = document.querySelector('.partner_license_num');
-let file = document.querySelector('.file');
+let partner_license_docs = document.querySelector('.partner_license_docs');
 let partnerFormBtn = document.querySelector('.partnerFormBtn');
 
 function nullCheck() {
@@ -32,13 +32,13 @@ function nullCheck() {
 		alert("사업자 번호를 입력해주세요.")
 		event.preventDefault();
 		partner_license_num.focus();
-	} else if(file.value==="") {
+	} else if(partner_license_docs.value ==="") {
 		alert("사업자 등록증을 첨부해주세요")
 		event.preventDefault();
-		file.focus();
+	
 	} 
+	
 }
-
 
 let leports_title = document.querySelector('.leports_title');
 let leports_content = document.querySelector('.leports_content');
